06d1a840cad5044ecc93d4a6862971881f7530a7
Rob Terhaar <rob@atlanticdynamic.com>
2013-06-14 18:30:35 -0400

    adding an optional preposition field, in between the target object and the action object

--- actstream/models.py
+++ actstream/models.py
@@ -96,6 +96,8 @@ class Action(models.Model):
 
     public = models.BooleanField(default=True)
 
+    preposition = models.CharField(max_length=32, blank=True, null=True)
+
     objects = actstream_settings.get_action_manager()
 
     class Meta:
